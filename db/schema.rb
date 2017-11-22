ffde41d4d443a6330d7273e7b06ce23151ea4756
kovals <kovals@bpol0419.emea.local>
2017-11-22 14:30:12 +0100

    addig attachments and starting with tags

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171122085352) do
+ActiveRecord::Schema.define(version: 20171122131933) do
+
+  create_table "pictures", force: :cascade do |t|
+    t.string "image"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "posts", force: :cascade do |t|
     t.string "title"
@@ -19,6 +25,12 @@ ActiveRecord::Schema.define(version: 20171122085352) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "image"
+  end
+
+  create_table "tags", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
 end
