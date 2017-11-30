106ddac3b36c840e31f99caa64e6afecc7f9cf94
kovals <kovals@bpol0419.emea.local>
2017-11-30 13:28:38 +0100

    done with categories

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,12 +1,12 @@
 class CategoriesController < ApplicationController
   before_action :set_category, only: [:show, :edit, :update, :destroy]
     
-    def index
-      @categories = Category.all
-    end
+  def index
+    @categories = Category.all
+  end
 
   def show
-    ##@posts = Post.where(category_id: [@category.subtree_ids]).paginate(page: params[:page], per_page: 5)
+    @posts = Post.where(category_id: [@category.subtree_ids]).paginate(page: params[:page], per_page: 5)
   end
   
   def new
